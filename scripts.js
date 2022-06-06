0312f81340acb01f5e0e945eb4a22a9302cade69
Stephamflavio <Stephdpmello@gmail.com>
2022-06-06 06:31:26 -0300

    construi uma logica de cadastro com condicionais js

---
+++ scripts.js
@@ -0,0 +1,32 @@
+// CADASTRO ------------------------------------------
+
+// const cadastro = () => {
+//     const usuario = []
+//     const nome = prompt("Digite seu nome") 
+//     const anoDeNascimento = Number(prompt("Digite seu ano de nascimento") )
+//     const nacionalidade = prompt("Digite sua nacionalidade") 
+//     const senha = prompt("Digite sua senha") 
+
+//     const idade = 2021 - anoDeNascimento
+
+//     if(senha.length < 6){
+//         console.log('sua senha deve ter pelo menos 6 caracteres')
+//     }
+//     if(idade < 18) {
+//         console.log('Para se cadastrar precisa ter 18 anos')
+//     }
+//     if(nacionalidade !== "brasileira") {
+//         console.log('Para se cadastrar precisa ser brasileiro')
+//     }
+
+//     if(idade >= 18 && nacionalidade === "brasileira" && senha.length >= 6){
+//         usuario.push(nome, anoDeNascimento, nacionalidade, senha)
+//         console.log('Usuário cadastrado com sucesso')
+//     }
+
+//     console.log("usuario: ", usuario)
+// }
+// console.log(cadastro());
+
+
+
